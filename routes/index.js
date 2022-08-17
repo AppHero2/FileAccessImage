7b4ad2f2679c7e58fb666f78e952242dff116706
AppHero <apphero.software@gmail.com>
2022-08-17 11:04:10 +0100

    display dropbox files

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+//first add the reference to the controller
+var controller = require('../controller');
+
+/* GET home page. */
+router.get('/', controller.home);
+
+router.get('/login', controller.login);
+
+router.get('/oauthredirect', controller.oauthredirect);
+
+module.exports = router;
