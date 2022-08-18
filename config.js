f7f5b7456dceefebee6336f14343363317c56ef4
AppHero <apphero.software@gmail.com>
2022-08-17 17:09:53 -0700

    update config uri

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   DBX_APP_KEY:'yay7g2fdg10fpd7',
   DBX_APP_SECRET:'jj6amcmd2fwnds6', 
   // OAUTH_REDIRECT_URL:"http://localhost:3000/oauthredirect",
-  OAUTH_REDIRECT_URL:"https://dashboard.heroku.com/apps/displaygallery/oauthredirect",
+  OAUTH_REDIRECT_URL:"https://displaygallery.herokuapp.com/oauthredirect",
   DBX_LIST_FOLDER_PATH:'/2/files/list_folder',
   DBX_LIST_FOLDER_CONTINUE_PATH:'/2/files/list_folder/continue',
   DBX_GET_TEMPORARY_LINK_PATH:'/2/files/get_temporary_link',
